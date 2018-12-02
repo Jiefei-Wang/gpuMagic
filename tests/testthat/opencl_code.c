813a4d2cfba16cb9830a37a193121123ccc5391b
Desktop <szwjf08@gmail.com>
2018-12-01 20:40:23 -0500

    unit test for kernel manager

--- tests/testthat/opencl_code.c
+++ tests/testthat/opencl_code.c
@@ -1,5 +1,4 @@
 kernel void vectorAdd(gAuto1* a,gAuto2* b,gAuto3* c){
   int i=get_global_id(0);
- c[i]=a[i]+b[i];
-  
+  c[i]=a[i]+b[i];
 }
---
+++ tests/testthat/test_kernelManager.R
@@ -0,0 +1,19 @@
+context("The functionality of the kernel manager")
+N_size=1000
+
+test_that("vector add",{
+  A=runif(N_size)
+  B=runif(N_size)
+  dev_C=gpuEmptMatrix(N_size,1)
+  
+  fileName<- 'opencl_code.c'
+  option=kernel.getOption()
+  
+  
+  .kernel(file=fileName,kernel="vectorAdd",parms=list(A,B,dev_C),.option=option)
+  
+  dev_C=download(dev_C)
+  C=as.vector(dev_C)
+  
+  expect_equal(sum(abs(C-A-B)),0)
+})
