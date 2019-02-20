f4fd8e4fd6df36a68f453c0246378b292c50a83b
Desktop <szwjf08@gmail.com>
2019-02-20 09:06:43 -0500

    update

--- R/gpuFunctions.R
+++ R/gpuFunctions.R
@@ -131,7 +131,7 @@ getCurDevice = function() {
 #' #Use the first device
 #' setDevice(1)
 #' #Use two devices
-#' setDevice(c(1,2))
+#' #setDevice(c(1,2))
 #' @return 'setDevice()': No return value
 #' @rdname DeviceInfo
 #' @export
